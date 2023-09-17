8663a187029af4d4e7a0edd4dda27ffa0940b5a8
Niklas <niklas_loos@web.de>
2023-09-17 13:24:32 +0200

    add display Level function

--- scripts/level-system.js
+++ scripts/level-system.js
@@ -1,11 +1,13 @@
-import { points } from '/script/point-system.js'
+import { points } from '/scripts/point-system.js'
 
 let currentLevel = 'superEasy';
+let currentLevelNumber = 1;
 
 export const levelInterval = () => {
+  displayLevel();
   setInterval(() => {
     if(currentLevel === 'superEasy') {
-      levelcheck(points, 5000, 'easy');
+      levelcheck(points, 500, 'easy');
     }
   }, 1000)
 } 
@@ -15,7 +17,12 @@ function levelcheck(ep, need, nextLevel) {
   if (ep > need) {
     console.log(`You are on level ${nextLevel} now!`);
     currentLevel = nextLevel;
-  } else {
-    console.log('Still on same level');
-  }
+    currentLevelNumber++;
+    displayLevel();
+  } 
+}
+
+function displayLevel() {
+  const levelcontainer = document.querySelector('.your-level-js');
+  levelcontainer.innerHTML = `Level ${currentLevelNumber} - ${currentLevel}`;
 }
