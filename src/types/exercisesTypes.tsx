0f26936d33e49a8ce5a323ea01a2fce1b2b7dcfd
Niklas <niklas_loos@web.de>
2023-10-18 07:40:25 +0200

    declare the types

---
+++ src/types/exercisesTypes.tsx
@@ -0,0 +1,8 @@
+export interface ExercisesTypes {
+  points: number;
+  setPoints: React.Dispatch<React.SetStateAction<number>>;
+  pointsWin: number;
+  setPointsWin: React.Dispatch<React.SetStateAction<number>>;
+  pointsLose: number;
+  setPointsLose: React.Dispatch<React.SetStateAction<number>>;
+}
---
+++ src/types/levelsTypes.tsx
@@ -0,0 +1,9 @@
+
+export interface LevelSystemTypes {
+  points: number;
+  setPoints: React.Dispatch<React.SetStateAction<number>>;
+  pointsWin: number;
+  setPointsWin: React.Dispatch<React.SetStateAction<number>>;
+  pointsLose: number;
+  setPointsLose: React.Dispatch<React.SetStateAction<number>>;
+}
